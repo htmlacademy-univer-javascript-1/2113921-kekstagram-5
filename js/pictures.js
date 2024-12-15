404d8cd402d4189e90453d18795f82bd28ad35fd
Alexey <alexeyf08@gmail.com>
2024-12-15 23:40:43 +0700

    Добавлен сценарий просмотра фотографий в полноразмерном режиме

--- js/pictures.js
+++ js/pictures.js
@@ -3,13 +3,14 @@ const container = document.querySelector(".pictures");
 const fragment = document.createDocumentFragment();
 
 const createPicture = (photo) => {
-  const {comments, description, likes, url} = photo;
+  const {comments, description, likes, url, id} = photo;
   const picture = pictureTemplate.cloneNode(true);
 
   picture.querySelector(".picture__img").src = url;
   picture.querySelector(".picture__img").alt = description;
   picture.querySelector(".picture__comments").textContent = comments.length;
   picture.querySelector(".picture__likes").textContent = likes;
+  picture.dataset.pictureId = id;
 
   return picture;
 };
